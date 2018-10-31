b03b2eefdec1930c277dd5c362ce8f09b5a1ee4f
Aparus <rustamapaev@gmail.com>
2018-10-31 22:46:54 +0300

    сделал полосу навигации

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import MainPage from "./components/mainPage/MainPage";
+import AuthorList from "./components/author/AuthorList";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route path="/authors" component={AuthorList} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
 
 const styles = {
   root: {
@@ -27,13 +28,29 @@ function Navbar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Typography variant="h6" color="inherit" className={classes.grow} />
+          <Button component={Link} to="/authors" color="inherit">
+            Authors
+          </Button>
+          <Button component={Link} to="/languages" color="inherit">
+            Languages
+          </Button>
+          <Button component={Link} to="/types" color="inherit">
+            Types
+          </Button>
+          <Button component={Link} to="/units" color="inherit">
+            Units
+          </Button>
+          <Button component={Link} to="/materials" color="inherit">
+            Materials
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
